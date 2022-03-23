e5003a3fbd306a16e9dc7efa877606494bad27b2
brunaguimaraesb <brunaguimaraes.santos@hotmail.com>
2022-03-23 17:26:06 -0300

    frontend

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function validar() {
+	
+	let nome = document.getElementById('nome').value;
+	let cpf = document.getElementById('cpf').value;
+	let contribuicao = document.getElementById('contribuicao').value;
+	
+}	
